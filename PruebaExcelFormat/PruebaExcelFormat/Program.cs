7b6cbc42a38da72f4307ea24a3e4b4237c5c7346
Edwinutec2017 <30840036+Edwinutec2017@users.noreply.github.com>
2020-01-13 13:45:05 -0600

    Archivos de ejemplo
    Eejemplo de uso de nuggets

---
+++ PruebaExcelFormat/PruebaExcelFormat/Program.cs
@@ -0,0 +1,51 @@
+﻿using ExcelNugget02;
+using ExcelNugget02.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace PruebaExcelFormat
+{
+    class Program
+    {
+        /*utiliza epplus.core para usar el nugget*/
+        static void Main(string[] args)
+        {
+            List<Prueba> lista = new List<Prueba>() {
+               new Prueba(){
+                   Name="edwin alexander nolasco1",
+                    edad=25,
+                   anio=2019,
+                   fecha=20191223,
+                   hora=1451
+
+               },
+                  new Prueba(){
+                   Name="edwin2",
+                   edad=25,
+                   anio=2019,
+                   fecha=20191223,
+                   hora=1451,
+                   descripcion="asasasa1s..............................."
+               },
+                      new Prueba(){
+                   Name="edwin3",
+                   edad=25,
+                   anio=2019,
+                   fecha=20191223,
+                   hora=1451,
+                   descripcion="a"
+               },
+           };
+
+            /*especificar donde se gurdar el documento*/
+            IExcel excel = new Excel();
+            var nombreArchivo = "Prueba";
+            /*especificar donde se gurdar el documento segun lo necesita ejemplo c:\*/
+            excel.GuardarArchivo(@"C:\excel\", nombreArchivo);
+            /*mandamos la lista de datos que queremos agregar al excel*/
+            var resp =  excel.NewContent(lista);
+            /*si la respuesta el tru el excel fue generado con exito */
+            Console.WriteLine(resp.Result);       
+        }
+    }
+}
---
+++ PruebaExcelFormat/PruebaExcelFormat/Prueba.cs
@@ -0,0 +1,23 @@
+﻿using ExcelNugget02;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaExcelFormat
+{
+    public class Prueba
+    {
+        [DescripcionExcel(Name = "NOMBRE", Ignore = false)]
+        public string Name { get; set; }
+        [DescripcionExcel(Name = "EDAD", Ignore = false)]
+        public int edad { get; set; }
+        [DescripcionExcel(Name = "AÑO", Ignore = false)]
+        public int anio { get; set; }
+        [DescripcionExcel(Name = "FECHA", Ignore = false)]
+        public int fecha { get; set; }
+        [DescripcionExcel(Name = "HORA", Ignore = false)]
+        public int hora { get; set; }
+        [DescripcionExcel(Name = "DESCRIPCION", Ignore = false)]
+        public string descripcion { get; set; }
+    }
+}
